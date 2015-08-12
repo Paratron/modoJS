7f480f5369dd9a2c37af87f1da877a4e3627afa1
Christian Engel <christian.engel@wearekiss.com>
2015-08-12 15:56:22 +0200

    Fixed a bug in modo.Dropdown that prevented models from collections to be selected

--- dist/modo-1.1.1-full.js
+++ dist/modo-1.1.1-full.js
@@ -4414,24 +4414,24 @@
  * @param params
  * @constructor
  */
-(function (){
+(function () {
 	'use strict';
 
 	var modoCore,
-		$;
+			$;
 
 	$ = jQuery;
 
 	//commonJS and AMD modularization - try to reach the core.
-	if(typeof modo !== 'undefined'){
+	if (typeof modo !== 'undefined') {
 		modoCore = modo;
 	} else {
-		if(typeof require === 'function'){
+		if (typeof require === 'function') {
 			modoCore = require('modo');
 		}
 	}
 
-	modoCore.defineElement('DropDown', ['dropdown', 'dropdown-button', 'dropdown-list', 'dropdown-dropped', 'dropdown-selected'], function (params){
+	modoCore.defineElement('DropDown', ['dropdown', 'dropdown-button', 'dropdown-list', 'dropdown-dropped', 'dropdown-selected'], function (params) {
 		params = params || {};
 
 		modoCore.Element.call(this, params);
@@ -4441,7 +4441,7 @@
 		var $button = $('<button></button>');
 		$button.addClass(modoCore.cssPrefix + modoCore.DropDown.classNames[1]);
 
-		if(params.tooltip){
+		if (params.tooltip) {
 			$button.attr('title', params.tooltip);
 		}
 
@@ -4449,12 +4449,12 @@
 
 		var settings = {
 			data: params.data,
-			buttonRender: params.buttonRender || function (d){
-				if(typeof d === 'string' || typeof d === 'number'){
+			buttonRender: params.buttonRender || function (d) {
+				if (typeof d === 'string' || typeof d === 'number') {
 					return d;
 				}
 				for (var key in d) {
-					if(key === '_m'){
+					if (key === '_m') {
 						continue;
 					}
 					break;
@@ -4463,18 +4463,18 @@
 			},
 			selectedItem: params.selectedItem !== undefined ? params.selectedItem : null,
 			selectedData: null,
-			placeholder:  params.placeholder || '',
-			keyboard:     params.keyboard || true
+			placeholder: params.placeholder || '',
+			keyboard: params.keyboard || true
 		};
 
-		if(params.keyboard){
-			if(!modo.keyListener){
+		if (params.keyboard) {
+			if (!modo.keyListener) {
 				throw new Error('keyListener missing');
 			}
 			modo.keyListener.enable();
 		}
 
-		if(typeof params.data === 'function'){
+		if (typeof params.data === 'function') {
 			settings.data = params.data();
 		}
 
@@ -4483,7 +4483,11 @@
 		var that = this;
 
 		params.itemEvents = {
-			'click': function (e, i){
+			'click': function (e, i, d) {
+				if (d instanceof Backbone.Model) {
+					that.set(d.id);
+					return;
+				}
 				that.set(i);
 			}
 		};
@@ -4493,23 +4497,23 @@
 		var dropList = new modoCore.List(params);
 
 
-		$button.on('click', function (e){
-			if(that.disabled){
+		$button.on('click', function (e) {
+			if (that.disabled) {
 				return;
 			}
 			that.el.addClass(modoCore.cssPrefix + modoCore.DropDown.classNames[3]);
-			if(settings.keyboard){
+			if (settings.keyboard) {
 				dropList.focus();
 			}
 			e.stopPropagation();
 
-			function clearFunc(e, key){
+			function clearFunc(e, key) {
 				e.preventDefault();
 				e.stopPropagation();
 
-				if(key === 'enter'){
+				if (key === 'enter') {
 					var selected = dropList.getFocusedIndex();
-					if(!selected){
+					if (!selected) {
 						return;
 					}
 					that.set(selected);
@@ -4540,9 +4544,9 @@
 		 * If you passed a Backbone Collection as data, pass a data id or cid.
 		 * @param {Integer|String} item
 		 */
-		this.set = function (item, options){
+		this.set = function (item, options) {
 			var silent,
-				prevItem;
+					prevItem;
 
 			options = options || {};
 
@@ -4551,15 +4555,15 @@
 			prevItem = this.selectedItem;
 			this.selectedItem = item;
 
-			if(item === null){
+			if (item === null) {
 				this.selectedData = null;
 				$button.html(settings.buttonRender(settings.placeholder));
 				this.trigger('change', null, null);
 				return;
 			}
 
-			if(settings.data instanceof Backbone.Collection){
-				if(!settings.data.get(item)){
+			if (settings.data instanceof Backbone.Collection) {
+				if (!settings.data.get(item)) {
 					this.selectedItem = prevItem;
 					throw new Error('Object not found');
 				}
@@ -4567,27 +4571,27 @@
 				this.selectedData = settings.data.get(item);
 				$button.html(settings.buttonRender(_.extend(this.selectedData.toJSON(), {'_m': this.selectedData}), this.selectedData));
 			} else {
-				if(typeof settings.data[item] === 'undefined'){
+				if (typeof settings.data[item] === 'undefined') {
 					this.selectedItem = prevItem;
 					throw new Error('Element "' + item + '" not found in dataset');
 				}
 				this.selectedData = settings.data[item];
 
-				if(this.selectedData instanceof Backbone.Model){
-					$button.html(settings.buttonRender(_.extend(this.selectedData.toJSON(), {'_m' : this.selectedData})));
+				if (this.selectedData instanceof Backbone.Model) {
+					$button.html(settings.buttonRender(_.extend(this.selectedData.toJSON(), {'_m': this.selectedData})));
 				} else {
 					$button.html(settings.buttonRender(this.selectedData));
 				}
 			}
 
-			if(!silent){
+			if (!silent) {
 				this.trigger('change', this.selectedItem, this.selectedData);
 			}
 
 			return this;
 		};
 
-		this.setDataset = function (dataset, options){
+		this.setDataset = function (dataset, options) {
 			settings.data = dataset;
 			dropList.set(dataset, options);
 			this.set(this.selectedItem, {silent: true});
@@ -4596,11 +4600,11 @@
 			return this;
 		};
 
-		this.get = function (){
+		this.get = function () {
 			return this.selectedItem;
 		};
 
-		this.getData = function (){
+		this.getData = function () {
 			return this.selectedData;
 		};
 
@@ -4608,7 +4612,7 @@
 
 		modoCore._stat('DropDown');
 	})
-		.inheritPrototype('Element');
+			.inheritPrototype('Element');
 
 	modoCore.DropDown.prototype.disable = modoCore.Button.prototype.disable;
 	modoCore.DropDown.prototype.enable = modoCore.Button.prototype.enable;
