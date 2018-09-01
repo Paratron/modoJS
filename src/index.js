9b6f59e587e8910457593b1e672760e05820145c
Christian Engel <christian.engel@wearekiss.com>
2018-09-01 13:19:39 +0200

    Update indexfile

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import Button from './Button';
+import ButtonGroup from './ButtonGroup';
+import Card from './Card';
+import Dialog from './Dialog/Dialog';
+import DialogContent from './Dialog/DialogContent';
+import DialogHeader from './Dialog/DialogHeader';
+import Icon from './Icon';
+import FormContainer from './FormContainer';
+import List from './List';
+import Table from './Table';
+import TextInput from './TextInput';
+import Toolbar from './Toolbar';
+
+import {
+	AppHeader,
+	Cell,
+	ContentCell,
+	Drawer,
+	Grid,
+	GridContainer,
+	GridFrame,
+	VerticalSpace,
+} from './layout';
+
+export {
+	Button,
+	ButtonGroup,
+	Card,
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	FormContainer,
+	Icon,
+	List,
+	Table,
+	TextInput,
+	Toolbar,
+	AppHeader,
+	Cell,
+	ContentCell,
+	Drawer,
+	Grid,
+	GridContainer,
+	GridFrame,
+	VerticalSpace,
+};
