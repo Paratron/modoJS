f6b47479bc3735f9f1b0531c29f35b2649ed60d5
Christian Engel <christian.engel@wearekiss.com>
2018-10-31 22:45:32 +0100

    Implement RadioButton and -Selector

---
+++ src/RadioButton/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RadioButton.scss';
+
+const propTypes = {
+	label: PropTypes.string,
+	tooltip: PropTypes.string,
+	enabled: PropTypes.bool,
+	className: PropTypes.string,
+	checked: PropTypes.bool,
+	onClick: PropTypes.func
+};
+
+const defaultProps = {
+	tooltip: '',
+	enabled: true,
+	className: '',
+	checked: false,
+	onClick: () => {
+	}
+};
+
+export default class RadioButton extends React.Component {
+	render() {
+		const classNames = ['mdo-radiobutton'];
+
+		const {
+			label,
+			tooltip,
+			enabled,
+			className,
+			checked,
+			onClick
+		} = this.props;
+
+		if(!enabled){
+			classNames.push('mdo-disabled');
+		}
+
+		if(checked){
+			classNames.push('mdo-active');
+		}
+
+		if(className){
+			classNames.push(className);
+		}
+
+		return (
+			<div onClick={onClick} className={classNames.join(' ')}>
+				<span title={tooltip}>{label}</span>
+			</div>
+		);
+	}
+}
+
+RadioButton.propTypes = propTypes;
+RadioButton.defaultProps = defaultProps;
---
+++ src/RadioSelector/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RadioButton from '../RadioButton';
+
+const propTypes = {
+	items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+	className: PropTypes.string,
+	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	onChange: PropTypes.func,
+	enabled: PropTypes.bool,
+};
+
+const defaultProps = {
+	className: '',
+	value: null,
+	enabled: true,
+	onChange: () => {},
+};
+
+export default class RadioSelector extends React.Component {
+	clickHandlers = {};
+
+	handleChange = (index) => {
+		const {
+			items,
+			onChange,
+		} = this.props;
+
+		const nextValue = items instanceof Array ? index : Object.keys(items)[index];
+
+		onChange(nextValue);
+	};
+
+	handleClick = (index) => {
+		const key = `${index}`;
+
+		if(this.clickHandlers[key]){
+			return this.clickHandlers[key];
+		}
+
+		const clickHandler = () => this.handleChange(index);
+
+		this.clickHandlers[key] = clickHandler;
+
+		return clickHandler;
+	};
+
+	render() {
+		const classNames = ['mdo-radioselector'];
+
+		const {
+			items,
+			className,
+			value,
+		} = this.props;
+
+		const isArr = items instanceof Array;
+		const itemLabels = isArr ? items : Object.values(items);
+		const itemKeys = isArr ? items.map((v, i) => i) : Object.keys(items);
+		const activeIndex = isArr ? value : itemKeys.indexOf(value);
+
+		if(className){
+			classNames.push(className);
+		}
+
+		return (
+			<div className={classNames.join(' ')}>
+				{ itemLabels.map((label, index) => <RadioButton
+					key={index}
+					label={label}
+					checked={index === activeIndex}
+					onClick={this.handleClick(index)}
+				/> ) }
+			</div>
+		);
+	}
+}
+
+RadioSelector.propTypes = propTypes;
+RadioSelector.defaultProps = defaultProps;
