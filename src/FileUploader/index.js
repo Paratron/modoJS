eab25aac9eb7677a50569f41ab6be844da9d1f09
Christian Engel <christian.engel@wearekiss.com>
2018-12-11 17:04:17 +0100

    Update FileUploader

--- src/FileUploader/index.js
+++ src/FileUploader/index.js
@@ -3,17 +3,34 @@ import PropTypes from 'prop-types';
 
 import Button from '../Button';
 
-const propTypes = {};
+const propTypes = {
+	value: PropTypes.string,
+	onChange: PropTypes.func,
+	filename: PropTypes.string,
+	targetURL: PropTypes.string,
+	accept: PropTypes.string,
+	enabled: PropTypes.bool,
+	onBeforeUpload: PropTypes.func,
+	onAfterUpload: PropTypes.func,
+};
+
+const defaultProps = {
+	enabled: true
+};
 
 const statusClasses = ['', 'mdo-uploading', 'mdo-upload-done', 'mdo-upload-error'];
 
-const FileUploader = ({value, onChange, filename, targetURL = '', accept}) => {
+const FileUploader = ({value, onChange, filename, targetURL = '', accept, enabled, onBeforeUpload, onAfterUpload}) => {
 	const classNames = ['mdo-fileUploader'];
 	const [filenameBuffer, setFilenameBuffer] = useState(filename);
 	const [uploadStatus, setUploadStatus] = useState(0);
 	const [progress, setProgress] = useState(0);
 
 	classNames.push(statusClasses[uploadStatus]);
+
+	if(!enabled){
+		classNames.push('mdo-disabled');
+	}
 
 	const handleFileSelect = (e) => {
 		const files = e.target.files;
@@ -33,14 +50,20 @@ const FileUploader = ({value, onChange, filename, targetURL = '', accept}) => {
 
 			xhr.addEventListener('load', () => {
 				setUploadStatus(2);
-
 				setTimeout(() => setUploadStatus(0), 2000);
+				if(onAfterUpload){
+					onAfterUpload(xhr.responseText);
+				}
 			});
 
 			xhr.addEventListener('error', (e) => {
 				console.log(e);
 				setUploadStatus(3);
 			});
+
+			if(onBeforeUpload){
+				onBeforeUpload();
+			}
 
 			xhr.open('POST', targetURL, true);
 			xhr.send(formData);
@@ -53,17 +76,18 @@ const FileUploader = ({value, onChange, filename, targetURL = '', accept}) => {
 			<Button
 				type={Button.TYPES.PRIMARY}
 				icon="cloud_upload"
-				enabled={uploadStatus === 0 || uploadStatus === 2}
+				enabled={enabled && (uploadStatus === 0 || uploadStatus === 2)}
 			>
 				Datei auswählen
 			</Button>
 			<span className="filename">{filenameBuffer}</span>
 			<span className="progress">{progress}%</span>
-			<input accept={accept} type="file" onChange={handleFileSelect}/>
+			{enabled && <input accept={accept} type="file" onChange={handleFileSelect}/>}
 		</div>
 	);
 };
 
 FileUploader.propTypes = propTypes;
+FileUploader.defaultProps = defaultProps;
 
 export default FileUploader;
